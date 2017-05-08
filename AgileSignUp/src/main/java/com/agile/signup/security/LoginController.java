8a099d5e5e29ba26db0c725fe8b072208b272d6e
Gonzalez <luciano.j.gonzalez@accenturefederal.com>
2017-05-08 08:07:42 -0400

    Changed credential verification to use database

--- AgileSignUp/src/main/java/com/agile/signup/security/LoginController.java
+++ AgileSignUp/src/main/java/com/agile/signup/security/LoginController.java
@@ -1,13 +1,21 @@
 package com.agile.signup.security;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.agile.signup.web.CourseController;
 
 @Controller
 public class LoginController {
+	
+	private static final Logger logger = LoggerFactory.getLogger(LoginController.class);
 	
 	 	@RequestMapping(value = "/login", method = RequestMethod.GET)
 	    public String login(Model model) {
